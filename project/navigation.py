b7de0b8a6777140d96edba6840e28577f0cae7ae
Aerin Brown <aerinebrown04@gmail.com>
2023-11-06 09:55:52 -0500

    feat: x-coordinate navigation

--- project/navigation.py
+++ project/navigation.py
@@ -1,4 +1,9 @@
+import supressants_roulette as sr
+import movement as move
+import line_tracking as lt
+
 cur_location = [0, 0]
+facing = 0 # Facing in pos x is 0, pos y is 1, neg x 2, neg y 3
 locations = []
 for i in range(3):
     x = "a"
@@ -10,3 +15,30 @@ for i in range(3):
         y = input(f"Y-Coordinate {i + 1}: ")
     y = int(x)
     locations.append([x, y])
+
+while len(locations) > 0:
+    destination = locations[0]
+
+    if destination[0] < locations[0]:
+        if facing == 1:
+            move.turn_90()
+        elif facing == 2:
+            move.turn_180()
+        elif facing == 3:
+            move.turn_90(False)
+        facing = 0
+    elif destination[0] > locations[0]:
+        if facing == 1:
+            move.turn_90(False)
+        elif facing == 3:
+            move.turn_90()
+        elif facing == 0:
+            move.turn_180()
+        facing = 2
+
+    while cur_location[0] != destination[0]:
+        lt.track_line(0)
+        cur_location[0] += 1
+        move.align_turn()
+
+sr.kill()
