bf51359e5eb2828d0db2169fe1ac2e45e72b4330
Aerin Brown <aerinebrown04@gmail.com>
2023-11-06 13:05:37 -0500

    refactor: hardcoded KNN

--- project/color_processing.py
+++ project/color_processing.py
@@ -1,11 +1,11 @@
 from utils.brick import EV3ColorSensor
-from sklearn.neighbors import KNeighborsClassifier
+import math
 
 #### GLOBAL VARIABLES ####
 
 CS = EV3ColorSensor(1)
-model = KNeighborsClassifier(weights="distance")
-distance_cap = 0.9 # Limit on model certainty. Subject to change during testing.
+color_centers = {}
+distance_cap = 10
 
 def train_model():
     """ Train KNN model based on data from csvs in the color_data folder
@@ -19,22 +19,29 @@ def train_model():
         model : Model
             The trained model
     """
-    data = []
-    labels = []
-
     for i, color in enumerate(["blue", "red", "green"]):
         with open(f"./color_data/{color}.csv", "r") as f:
+            red_sum = 0
+            green_sum = 0
+            blue_sum = 0
+            n_points = 0
             for line in f.readlines():
-                point = line.strip("[]").split(",")
+                point = line.strip("[]").split(",")[:3]
                 for i in range(len(point)):
                     point[i] = int(point[i])
-                data.append(point)
-                labels.append(i)
-    
-    model.fit(data, labels)
-    return model
+                total = sum(point)
+                for i in range(len(point)):
+                    point[i] = point[i] / total
+                red_sum += point[0]
+                green_sum += point[1]
+                blue_sum += point[2]
+                n += 1
+            color_centers[color] = [red_sum / n_points, green_sum / n_points, blue_sum / n_points]
+            
+            
+                
 
-def classify(point, model):
+def classify(point):
     """ Classifies an rgb point as blue, red, or green. Classifies as other if confidence on that
     point is beyond the threshold given by distance_cap.
 
@@ -50,8 +57,20 @@ def classify(point, model):
         color: int
             0 for blue, 1 for red, 2 for green, or 3 for other.
     """
-    probabilities = model.predict_proba([point])
-    confidence = max(probabilities)
-    if confidence < distance_cap:
+    red_distance = math.sqrt(sum(math.pow(point[i] - color_centers["red"][i], 2) for i in range(len(3))))
+    green_distance = math.sqrt(sum(math.pow(point[i] - color_centers["green"][i], 2) for i in range(len(3))))
+    blue_distance = math.sqrt(sum(math.pow(point[i] - color_centers["blue"][i], 2) for i in range(len(3))))
+    
+    if red_distance < green_distance and red_distance < blue_distance:
+        if red_distance > distance_cap:
+            return 3
+        return 1
+    elif blue_distance < green_distance and blue_distance < red_distance:
+        if blue_distance > distance_cap:
+            return 3
+        return 0
+    elif green_distance > distance_cap:
         return 3
-    return probabilities.index(confidence)
+    return 2
+
+    
