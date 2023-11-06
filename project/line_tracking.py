265bed069409f79aa6d3182f7763cba187e950d0
Aerin Brown <aerinebrown04@gmail.com>
2023-11-06 09:34:31 -0500

    feat: line tracking algorithm

--- project/line_tracking.py
+++ project/line_tracking.py
@@ -3,16 +3,53 @@
 """
 This code is used to detect the color of the line and output the data to the different subsystems.
 """
+from color_processing import classify
+from utils.brick import EV3ColorSensor, wait_ready_sensors, Motor
+from time import sleep
 
-# Add your imports here, if any
-from utils.brick import EV3ColorSensor, wait_ready_sensors
+#### GLOBAL VARIABLES ####
 
-
-# complete this based on your hardware setup
 COLOR_SENSOR = EV3ColorSensor(...)
 SENSOR_POLL_SLEEP = 0.05
 
+LEFT_MOTOR = Motor("A")
+RIGHT_MOTOR = Motor("B") 
+
+LT_HIGH_POWER = 20
+LT_LOW_POWER = 18
+
 wait_ready_sensors(True) # Input True to see what the robot is trying to initialize! False to be silent.
+
+def track_line(color):
+    """
+    Runs an infinite loop to follow a colored line until a green block is reached. 
+    Does so by driving at a very slight angle until the line is no longer in sight, then switching direction,
+    and repeating.
+
+    Params
+    ------
+        color : int
+            The color of the line in question - 0 for blue, 1 for red
+    
+    Returns
+    -------
+        None
+    """
+    color_detected = color
+    LEFT_MOTOR.set_power(LT_HIGH_POWER)
+    RIGHT_MOTOR.set_power(LT_LOW_POWER)
+    while color_detected != 2:
+        sleep(SENSOR_POLL_SLEEP)
+        color_detected = classify(COLOR_SENSOR.get_value())
+        if color_detected != color:
+            if LEFT_MOTOR.get_power() == LT_HIGH_POWER:
+                LEFT_MOTOR.set_power(LT_LOW_POWER)
+                RIGHT_MOTOR.set_power(LT_HIGH_POWER)
+            else:
+                LEFT_MOTOR.set_power(LT_HIGH_POWER)
+                RIGHT_MOTOR.set_power(LT_LOW_POWER)
+    LEFT_MOTOR.set_power(0)
+    RIGHT_MOTOR.set_power(0)
 
 
 def determine_color():
