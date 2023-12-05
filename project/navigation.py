583f434f957ac3d94fd632f291bd95d97d8d279d
Aerin Brown <aerinebrown04@gmail.com>
2023-12-04 19:12:38 -0500

    feat: reruns

--- project/navigation.py
+++ project/navigation.py
@@ -59,113 +59,120 @@ cur_location = [0, 0]
 prev = [0, 0]
 blocked = [[False for i in range(4)] for j in range(4)]
 facing = 0 # Facing in pos x is 0, pos y is 1, neg x 2, neg y 3
-locations = []
-colors = ["D", "F", "B", "C", "E", "A"]
-fires = input("Enter fire locations and suppressant types: ").split(",")
-blocked[int(fires[0])][int(fires[1])] = True
-blocked[int(fires[3])][int(fires[4])] = True
-blocked[int(fires[6])][int(fires[7])] = True
-for i in range(3):
-    x = int(fires[i * 3])
-    y = int(fires[i * 3 +1])
-    color = fires[i * 3 +2]
-    locations.extend(calc_path(blocked, prev, [x, y, color]))
-    prev = locations[-2]
-    blocked[x][y] = True
-    locations.append(prev)
-locations.extend(calc_path(blocked, prev, [0, 0, "None"]))
+cont = True
 
-color = 0
-align = False
-while len(locations) > 0:
-    destination = locations[0]
+while cont:
+    locations = []
+    colors = ["D", "F", "B", "C", "E", "A"]
+    fires = input("Enter fire locations and suppressant types: ").split(",")
+    blocked[int(fires[0])][int(fires[1])] = True
+    blocked[int(fires[3])][int(fires[4])] = True
+    blocked[int(fires[6])][int(fires[7])] = True
+    for i in range(3):
+        x = int(fires[i * 3])
+        y = int(fires[i * 3 +1])
+        color = fires[i * 3 +2]
+        locations.extend(calc_path(blocked, prev, [x, y, color]))
+        prev = locations[-2]
+        blocked[x][y] = True
+        locations.append(prev)
+    locations.extend(calc_path(blocked, prev, [0, 0, "None"]))
 
-    if cur_location[0] < destination[0]:
-        if align:
-            move.align_turn()
-            align = False
-        if facing == 1:
-            move.turn_90(False)
-        elif facing == 2:
-            move.turn_180()
-        elif facing == 3:
-            move.turn_90()
-        facing = 0
-    elif cur_location[0] > destination[0]:
-        if align:
-            move.align_turn()
-            align = False
-        if facing == 1:
-            move.turn_90()
-        elif facing == 3:
-            move.turn_90(False)
-        elif facing == 0:
-            move.turn_180()
-        facing = 2
-    
-    while cur_location[0] != destination[0]:
-        if align:
-            move.align_turn()
-            align = False
-        lt.track_line(color_centers)
-        if facing == 0:
-            cur_location[0] += 1
-        else:
-            cur_location[0] -= 1
-        align = True
-        
+    color = 0
+    align = False
+    while len(locations) > 0:
+        destination = locations[0]
 
-    if cur_location[1] < destination[1]:
-        if align:
-            move.align_turn()
-            align = False
-        if facing == 0:
-            move.turn_90()
-        elif facing == 2:
-            move.turn_90(False)
-        elif facing == 3:
-            move.turn_180()
-        facing = 1
-    elif cur_location[1] > destination[1]:
-        if align:
-            move.align_turn()
-            align = False
-        if facing == 0:
-            move.turn_90(False)
-        elif facing == 1:
-            move.turn_180()
-        elif facing == 2:
-            move.turn_90()
-        facing = 3
-    
-    while cur_location[1] != destination[1]:
-        if align:
-            move.align_turn()
-            align = False
-        lt.track_line(color_centers)
-        if facing == 1:
-            cur_location[1] += 1
-        else:
-            cur_location[1] -= 1
-        align = True
-
-    if destination[2] in colors:
-        align = False
-        color = colors.index(destination[2])
-        move.turn_180()
-        if facing == 0:
-            facing = 2
-        elif facing == 1:
-            facing = 3
-        elif facing == 2:
+        if cur_location[0] < destination[0]:
+            if align:
+                move.align_turn()
+                align = False
+            if facing == 1:
+                move.turn_90(False)
+            elif facing == 2:
+                move.turn_180()
+            elif facing == 3:
+                move.turn_90()
             facing = 0
-        else:
-            facing = 1
-        move.increment_forward()
-        sr.select_block(color)
-    
-    locations.pop(0)
+        elif cur_location[0] > destination[0]:
+            if align:
+                move.align_turn()
+                align = False
+            if facing == 1:
+                move.turn_90()
+            elif facing == 3:
+                move.turn_90(False)
+            elif facing == 0:
+                move.turn_180()
+            facing = 2
+        
+        while cur_location[0] != destination[0]:
+            if align:
+                move.align_turn()
+                align = False
+            lt.track_line(color_centers)
+            if facing == 0:
+                cur_location[0] += 1
+            else:
+                cur_location[0] -= 1
+            align = True
+            
 
-move.align_turn()
-move.stop()
+        if cur_location[1] < destination[1]:
+            if align:
+                move.align_turn()
+                align = False
+            if facing == 0:
+                move.turn_90()
+            elif facing == 2:
+                move.turn_90(False)
+            elif facing == 3:
+                move.turn_180()
+            facing = 1
+        elif cur_location[1] > destination[1]:
+            if align:
+                move.align_turn()
+                align = False
+            if facing == 0:
+                move.turn_90(False)
+            elif facing == 1:
+                move.turn_180()
+            elif facing == 2:
+                move.turn_90()
+            facing = 3
+        
+        while cur_location[1] != destination[1]:
+            if align:
+                move.align_turn()
+                align = False
+            lt.track_line(color_centers)
+            if facing == 1:
+                cur_location[1] += 1
+            else:
+                cur_location[1] -= 1
+            align = True
+
+        if destination[2] in colors:
+            align = False
+            color = colors.index(destination[2])
+            move.turn_180()
+            if facing == 0:
+                facing = 2
+            elif facing == 1:
+                facing = 3
+            elif facing == 2:
+                facing = 0
+            else:
+                facing = 1
+            move.increment_forward()
+            sr.select_block(color)
+        
+        locations.pop(0)
+
+    move.align_turn()
+    move.stop()
+
+    cont_str = input("Perform another run? [Y/N] ").lower()
+    cont = cont_str == "y"
+    
 sr.kill()
